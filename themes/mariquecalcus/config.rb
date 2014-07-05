f63001cec3e7818b099cff0854f7df9000e5e3cc
targoo <targoo@gmail.com>
2014-07-05 22:16:13 +0100

    clean up

--- themes/mariquecalcus/config.rb
+++ themes/mariquecalcus/config.rb
@@ -14,6 +14,7 @@ http_path = "/themes/mariquecalcus"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
